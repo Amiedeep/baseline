53838ebe0ca1ab22b63e5636c970422c6d267975
Amandeep Singh <amandees@thoughtworks.com>
2015-08-24 17:42:53 +0530

    taking input from user..

--- src/CalculateSaleTax.java
+++ src/CalculateSaleTax.java
@@ -1,14 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by amandeepsingh on 24/08/15.
  */
 public class CalculateSaleTax {
-
+    private Scanner sc;
+    private ArrayList<String> items;
     public CalculateSaleTax() {
-
+        sc = new Scanner(System.in);
+        items = new ArrayList<>();
     }
 
     public static void main(String[] args) {
         CalculateSaleTax calculateSaleTax = new CalculateSaleTax();
+        calculateSaleTax.takeUserInput();
+    }
 
+    private void takeUserInput() {
+        System.out.println("How many items to buy?");
+        int numberOfItems = sc.nextInt();
+        for (int i = 0; i < numberOfItems ; i++) {
+            items.add(sc.nextLine());
+        }
+
+
+//        for (int i = 0; i < numberOfItems; i++) {
+//            System.out.println(items.get(i));
+//        }
     }
 }
