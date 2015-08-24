87bcc9f21f97b2d09d97f9e609c530c7580c2a9a
Amandeep Singh <amandees@thoughtworks.com>
2015-08-24 17:33:32 +0530

    started working on it..added new CalculateSaleTax for starting

---
+++ src/CalculateSaleTax.java
@@ -0,0 +1,14 @@
+/**
+ * Created by amandeepsingh on 24/08/15.
+ */
+public class CalculateSaleTax {
+
+    public CalculateSaleTax() {
+
+    }
+
+    public static void main(String[] args) {
+        CalculateSaleTax calculateSaleTax = new CalculateSaleTax();
+
+    }
+}
